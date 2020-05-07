67707a544c616db648860fc8936f86bf70396fc0
NAKANO Hideo <pinarello.marvel@gmail.com>
2020-05-07 19:36:53 +0900

    fixes help message

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
         (author: "NAKANO Hideo <pinarello.marvel@gmail.com>")
         (about: "Twitter timeline update notification")
         (@subcommand init =>
-            (about: "initialize environment")
+            (about: "Initializes environment")
             (@arg consumer_key: +required "consumer key")
             (@arg consumer_secret: --secret +takes_value "consumer secret")
             (@arg database_file: --db +takes_value "database file")
@@ -29,20 +29,20 @@ fn main() {
             (@arg gmail_password: --gmail_password +takes_value "gmail password")
         )
         (@subcommand add =>
-            (about: "add screen name to watch updates")
+            (about: "Adds screen name to watch updates")
             (@arg screen_name: +required "screen name")
         )
         (@subcommand list =>
-            (about: "list tweets collected currently")
+            (about: "Lists tweets collected currently")
             (@arg screen_name: "screen name")
             (@arg max_count: --max +takes_value "max count of tweet [default is 10]")
         )
         (@subcommand remove =>
-            (about: "remove screen name watching updates")
+            (about: "Removes screen name watching updates")
             (@arg screen_name: +required "screen name")
         )
         (@subcommand check_update =>
-            (about: "remove screen name watching updates")
+            (about: "Checks timeline updates and notify updated tweets")
             (@arg screen_name: "screen name")
         )
     ).get_matches();
